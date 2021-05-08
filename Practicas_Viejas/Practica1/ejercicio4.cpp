fa0cfedd782eb12d76191669f1276e4f80f63769
Carlos Monsalve <letcarlosin@gmail.com>
2021-05-07 23:10:13 -0400

    Cambios de la noche del 7-5-2021

--- Practicas_Viejas/Practica1/ejercicio4.cpp
+++ Practicas_Viejas/Practica1/ejercicio4.cpp
@@ -19,18 +19,18 @@ int main(){
 }
 
 void main2() {
-      q = (*q).next;
-      *q = *((*q).next);
-      (*q).next;
-      (*((*q).next)).next
-      q = (*r).next;
-      *q = *((*r).next);
-      (*s).next = (*q).next;
-      (*s).next = s;
-      t = *q;
-      *q = *s;
-      *s = t;
-      q = t;
-      (*r).next = q;
-      (*((*((*q).next)).next)).next;
+      q = (*q).next; //Q toma el valor de su posición siguiente, es decir 3.
+      *q = *((*q).next); //q toma el valor de su posicion siguiente, es decir 4
+      (*q).next; //No pasa nada
+      (*((*q).next)).next; //No pasa nada
+      q = (*r).next; //El siguiente de r es nulo, asi que q también. 
+      *q = *((*r).next); //Igual que el valor contenido en q
+      (*s).next = (*q).next; //El siguiente de s es igualado a nulo ya que q esta en nulo
+      (*s).next = s; //el siguiente de s pasa a tener el mismo valor que s, es decir 5
+      t = *q; //t pasa a valer nulo como q.
+      *q = *s; //q pasa a valer 5 como s.
+      *s = t; //s pasa a valer nulo
+      //q = t; //esto no es válido porque t no es un puntero como q
+      (*r).next = q; //el siguiente de r vale 5 ahora como 5 y tiene la misma posicion de memoria
+      (*((*((*q).next)).next)).next; //supongo q no pasa naa
 }
